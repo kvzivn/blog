d8df92d80cfe0a46a2652b5a3f31f35580b74fc0
Kevin Ivan <kevin.ivan@apsis.com>
2015-10-12 23:06:33 +0200

    update ze gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,12 @@ import gulp from 'gulp';
 import stylus from 'gulp-stylus';
 import nano from 'gulp-cssnano';
 import browserSync from 'browser-sync';
+import wait from 'gulp-wait';
 import run from 'gulp-run';
 
 const reload = browserSync.reload;
+
+
 
 gulp.task('bs', ['styles'], () => {
     browserSync({
@@ -14,20 +17,26 @@ gulp.task('bs', ['styles'], () => {
         browser: ['google chrome']
     });
 
-    gulp.watch(['src/**/*.md'], ['metalsmith', reload]);
-    gulp.watch(['layouts/**/*.hbt'], ['metalsmith', reload]);
-    gulp.watch(['src/styles/*.styl'], ['styles', reload]);
+    gulp.watch('src/**/*.md', ['metalsmith']);
+    gulp.watch('layouts/**/*.hbt', ['metalsmith']);
+    gulp.watch('src/stylesheets/*.styl', ['styles', reload]);
 });
 
+
+
 gulp.task('styles', () => {
-    return gulp.src('src/styles/styles.styl')
+    return gulp.src('src/stylesheets/styles.styl')
         .pipe(stylus())
         .pipe(nano())
         .pipe(gulp.dest('dist/css'))
 });
 
+
+
 gulp.task('metalsmith', () => {
     run('node --harmony index').exec()
 });
+
+
 
 gulp.task('default', ['bs']);
